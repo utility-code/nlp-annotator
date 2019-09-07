1e0e854cfd166b4a6245a2180d19ba74b9a27536
Subhaditya <msubhaditya@gmail.com>
2019-09-07 18:54:19 +0530

    Add and finish features

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 - Make life easier 
 - FREE 
 - Does what it says 
+
+
+### How to make it work?
+- Just open the html file on any browser (Chrome pref)
+- Follow the instructions
+
+> Would be great if fellow devs could extend this and help out the community. Open Source.
--- funcs.js
+++ funcs.js
@@ -1,9 +1,54 @@
-function GetSelectedText () {
-    console.log(document.getElementById('tag').value);
-            if (window.getSelection) {
-                var selRange = window.getSelection();
+let count = -1;
+let sente = new Array();
+let finJson = {};
+function GetTag()
+{
 
-                console.log(selRange.getRangeAt(0).startOffset);
-                console.log(selRange.getRangeAt(0).endOffset);
-            }
+    // console.log(document.getElementById('split').value);
+    sente = document.getElementById('sent').value.trim().split("\n");
+    let tag_name = document.getElementById('tag').value.trim();
+    let max_li = sente.length;
+    console.log(sente);
+    console.log(tag_name);
+    document.getElementById('Information').innerHTML = "We have split your text. Now Lets begin!";
+    document.getElementById('startrun').innerHTML = "Click to continue";
+    // let count = 0;
+
+
+
+}
+
+
+function GetSelectedText()
+{
+    if(count!=-1){
+        document.getElementById('startrun').innerHTML = "Click to continue";
+    }
+    if (window.getSelection)
+    {
+        var selRange = window.getSelection();
+
+        console.log(selRange.toString(),selRange.getRangeAt(0).startOffset,selRange.getRangeAt(0).endOffset);
+        if (count!=-1){
+            let sf = selRange.getRangeAt(0).startOffset-12;
+            let ef = selRange.getRangeAt(0).endOffset-13;
+            document.getElementById('textshow').innerHTML = "Prev selection: \t" + sente[count]+" "+selRange.toString()+" "+sf+" "+ef;
+            finJson[sente[count]] = {"word":selRange.toString(), "start":sf, "end":ef};
         }
+
+        count+=1;
+
+        if(count<sente.length){
+
+            document.getElementById('text').innerHTML = "Sentence "+(count+1)+": "+sente[count];
+
+        }else if(count>=sente.length){
+            document.getElementById('textshow').innerHTML = "Check below";
+            document.getElementById('text').innerHTML = sente[count];
+            document.getElementById('jsonnow').innerHTML = JSON.stringify( finJson);
+            // console.log(finJson);
+        }
+
+
+    }
+}
